c1178651273af1ca3004edbd6add23f0a31c09b0
Hydroplane72 <mrrtrombone@gmail.com>
2017-12-20 19:39:08 -0600

    Recreate Main website added Datamodel for santa helper

--- matthewrozendaal.com/App_Start/BundleConfig.cs
+++ matthewrozendaal.com/App_Start/BundleConfig.cs
@@ -23,9 +23,14 @@ namespace matthewrozendaal.com
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/customFormating.css",
+                       "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/background.css",
+                      "~/Content/menuHover.css"
+                      ));
         }
     }
 }
---
+++ matthewrozendaal.com/Controllers/Santa_Helper/KidController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace matthewrozendaal.com.Controllers.Santa_Helper
+{
+    public class KidController : Controller
+    {
+        // GET: Kid
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
